f3f4acd619bfe7228f4a280ed870974b3669934a
dungnt07 <dung.nt205148@sis.hust.edu.vn>
2022-07-11 16:14:58 +0700

    clean up

--- src/tourism/entity/IQueryable.java
+++ src/tourism/entity/IQueryable.java
@@ -6,15 +6,11 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.rdf.model.Model;
 
 public interface IQueryable {
-	// Obtain class-specific query string
 	public String queryString();
 	
-	// Get results and write to file
-	public void queryToFile() throws IOException;
-	
-	// Get instance of an object for each result
 	public IQueryable getObjectFromQuery(QuerySolution qs) throws IOException;
 	
-	// Create and return a model containing a triple from obtained object
 	public Model getModelInstance(IQueryable queryableEntity);
+	
+	public void queryToFile() throws IOException;
 }
